760c8e7c45061e4e9f73f6363fba15fbf7dba149
Hp <Hp@DESKTOP-ID5FAVQ>
2022-11-01 09:44:22 +0100

    tache6

--- TP2 Csharp/Program.cs
+++ TP2 Csharp/Program.cs
@@ -1,13 +1,26 @@
 ﻿
- public  abstract class Examen
+using System.Security.Cryptography.X509Certificates;
+
+public  abstract class Examen
 { 
     private int date_debut ;
+    private string lieu;
+    private int note;
 
     public Examen(int date_debut) 
     {
         this.date_debut = date_debut;
     
-    
+    }
+
+    public void set_lieu( string nouveau_lieu)
+    {
+        this.lieu = nouveau_lieu;
+    }
+
+    public void set_note( int nouveau_note)
+    {
+        note = nouveau_note;
     }
 
     public double getmesure_duree( int date_fin)
@@ -56,7 +69,10 @@ public class Reponse
 public class Proposition
 {
     private int nbr_proposition;
-    public Proposition() { }
+    public Proposition( int nbr_proposition) 
+    { 
+        this.nbr_proposition = nbr_proposition;
+    }
 
      public int getnbr_proposition()
     {
