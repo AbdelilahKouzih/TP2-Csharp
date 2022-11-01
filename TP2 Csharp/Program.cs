72a333965a3d4923cbd567f9b28d55d0ea038df7
Hp <Hp@DESKTOP-ID5FAVQ>
2022-11-01 08:37:34 +0100

    tache1

--- TP2 Csharp/Program.cs
+++ TP2 Csharp/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+ public class Examen
+{ 
+    private string date_debut ;
+
+    public Examen(string date_debut) 
+    {
+        this.date_debut = date_debut;
+    
+    
+    }
+}
+
+public class Question : Examen
+{
+    private string type;
+
+
+    public void repondre()
+    {
+        
+    }
+
+
+}
+
+public class Reponse
+{
+    private string degre_certitude;
+
+
+}
+
